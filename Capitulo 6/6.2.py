147710cb0f3e9d26c67be6f50d1c5839718bae76
Paulo Henrique Archilha de Oliveira <112733383+PauloArchilha@users.noreply.github.com>
2022-10-13 19:19:59 -0300

    Update 6.2.py

--- Capitulo 6/6.2.py
+++ Capitulo 6/6.2.py
@@ -1,4 +1,4 @@
 favorite_numbers = {'paulo': 23, 'mari': 13 , 'menor': 22, 'artur': 22,}
 
 for k, v in favorite_numbers.items():
-    print(f'O número favorito do {k.title()} é {v}')
+    print(f'O número favorito de {k.title()} é {v}')
