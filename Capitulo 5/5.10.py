22695b80aa4d0547b364464ed36b1fa5545d202a
Paulo Henrique Archilha de Oliveira <112733383+PauloArchilha@users.noreply.github.com>
2022-10-13 12:33:01 -0300

    Update 5.10.py

--- Capitulo 5/5.10.py
+++ Capitulo 5/5.10.py
@@ -1,1 +1,9 @@
+current_users = ['paulo', 'mari', 'menor', 'artur', 'admin']
+new_users = ['bruna', 'tairine', 'artur', 'MARI', 'lucas']
 
+
+for user in new_users:
+    if user.lower() in current_users:
+        print(f'Nome: {user.title()} já cadastrado')
+    else:
+        print(f'Nome: {user.title()} cadastrado com sucesso!')
