179000641a1e905b058d02d95ed14ce0eb8becbe
Paulo Henrique Archilha de Oliveira <112733383+PauloArchilha@users.noreply.github.com>
2022-10-13 19:14:05 -0300

    Create 6.1.py

---
+++ Capitulo 6/6.1.py
@@ -0,0 +1,3 @@
+people = {'first_name': 'Paulo', 'last_name': 'Archilha',
+          'age': 31, 'city': 'Severinia'}
+print(f'O nome é {people["first_name"]}, o sobrenome é {people["last_name"]}, a idade é {people["age"]} e a cidade é {people["city"]}')
