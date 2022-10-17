12052e0eb00703cba2135ddc1b69011e0d609008
pauloarchilha <pauloarchilha@gmail.com>
2022-10-17 12:43:41 -0300

    edit_1

--- Capitulo 6/6.7.py
+++ Capitulo 6/6.7.py
@@ -2,11 +2,12 @@ people_1 = {'first': 'Paulo', 'last': 'Archilha',
             'age': 31, 'city': 'Severinia'}
 people_2 = {'first': 'Mari', 'last': 'Pereira',
             'age': 22, 'city': 'Severinia'}
-people_3 = {'first': 'Artur', 'last': 'Haines',
+people_3 = {'first': 'Jose', 'last': 'Oliveira',
             'age': 29, 'city': 'SP'}
 
 peoples = [people_1, people_2, people_3]
 
 for people in peoples:
     full_name = people['first'] + ' ' + people['last']
-    print(f'Nome completo: {full_name} , idade: {people["age"]}, cidade: {people["city"]}.')
+    print(
+        f'Nome completo: {full_name} , idade: {people["age"]}, cidade: {people["city"]}.')
